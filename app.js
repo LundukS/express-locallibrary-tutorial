3712fd29c5f652faee408032a4f1ce6f2157a908
Lunduk <59974861+LundukS@users.noreply.github.com>
2020-05-04 00:25:58 -0500

    Data

--- app.js
+++ app.js
@@ -3,13 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-
-
-
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
 
 let app = express();
 
