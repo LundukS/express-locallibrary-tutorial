bda806eab7400302410126eceb6c72050477c16a
Lunduk <59974861+LundukS@users.noreply.github.com>
2020-05-04 11:47:30 -0500

    Controllers

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const dotenv = require('dotenv')
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 let app = express();
 
+dotenv.config({ path: '.env' });
+
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://lunduk:sherpa@cluster0-knecq.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+const mongoDB = process.env.ATLAS_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true ,useUnifiedTopology: true });
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -27,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- models/author.js
+++ models/author.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const moment = require('./node_modules/moment'); // For date handling.
+const moment = require('moment'); // For date handling.
 
 let Schema = mongoose.Schema;
 
